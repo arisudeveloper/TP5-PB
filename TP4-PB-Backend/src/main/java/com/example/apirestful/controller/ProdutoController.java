647d723ee13e3ee7c8c4f144ac638a1eb08801cd
arisudeveloper <alicesantossilva345@gmail.com>
2026-03-31 11:07:24 -0300

    CrossOrigin alterado

--- TP4-PB-Backend/src/main/java/com/example/apirestful/controller/ProdutoController.java
+++ TP4-PB-Backend/src/main/java/com/example/apirestful/controller/ProdutoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
